472d05203e773010ed240f6cf5751b8af4fc0297
AntonKobyshev <antonkobyshev@gmail.com>
2023-02-18 14:17:08 +0200

    Update task-01.js
    Task 1

---
+++ js/task-01.js
@@ -0,0 +1,28 @@
+// Визначаємо загальну кількість категорій(айтемів)
+
+const allItems = document.querySelectorAll(".item");
+console.log(`Number of categories: ${allItems.length} `);
+
+// Перебираємо та визначаємо кількість елементів на кожній ітерації
+
+// Спосіб 1
+
+const itemsArray = [...allItems]
+  .map(
+    categories => `Category: ${categories.firstElementChild.textContent}
+Elements: ${categories.children[1].children.length}`
+  )
+  .join("\n \n");
+console.log(itemsArray);
+
+// Спосіб 2
+
+Array.prototype.forEach.call(allItems, (element) => {
+  const itemTitle = element.querySelector('h2').textContent;
+  const itemLength = element.querySelectorAll('li').length;
+  
+  console.log(
+    `Category: ${itemTitle} \nElements: ${itemLength} \n `);
+});
+
+
